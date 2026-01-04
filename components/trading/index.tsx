2f9d2815c4c015f5eec8d1e5a6344a70551a6b0a
Ivan Molto <ivanmolto@gmail.com>
2026-01-04 10:32:16 +0100

    feat: Export trading-related components including TradingPanel, SharesInput, PriceImpact, ConfirmDialog, and PositionCard from trading index

---
+++ components/trading/index.tsx
@@ -0,0 +1,5 @@
+export { TradingPanel } from "./TradingPanel";
+export { SharesInput } from "./SharesInput";
+export { PriceImpact, PriceImpactDetails } from "./PriceImpact";
+export { ConfirmDialog } from "./ConfirmDialog";
+export { PositionCard, NoPositions } from "./PositionCard";
